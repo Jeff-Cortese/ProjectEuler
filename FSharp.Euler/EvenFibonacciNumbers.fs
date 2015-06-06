1ad2eb55d48d2ad54df707529fdfbf249631ad3e
Jeff Cortese <JCortese86@gmail.com>
2015-06-05 19:12:25 -0500

    Initial commit

---
+++ FSharp.Euler/EvenFibonacciNumbers.fs
@@ -0,0 +1,27 @@
+﻿// https://projecteuler.net/problem=2
+
+module FSharp.Euler.EvenFibonacciNumbers
+
+
+let isEven x =
+    x % 2 = 0
+
+
+let getFibsUntil predicate =
+    let rec getFibsInner previous i fibs =
+        let next = previous + i
+
+        if not (predicate i) 
+        then fibs
+        else getFibsInner i next (next :: fibs)
+    
+    getFibsInner 0 1 []
+
+
+let sumOfEvenFibNumbersTo sumTo =
+    getFibsUntil (fun i -> i < sumTo)
+    |> Seq.filter isEven
+    |> Seq.sum
+
+
+let answer = sumOfEvenFibNumbersTo 4000000 //4613732
---
+++ FSharp.Euler/MultiplesOf3And5.fs
@@ -0,0 +1,42 @@
+﻿// https://projecteuler.net/problem=1
+
+module FSharp.Euler.MultiplesOf3And5
+
+
+let isMultipleOf number multiple = 
+    number % multiple = 0
+
+
+let isMultipleOfAnyIn list x =
+    list |> Seq.exists (isMultipleOf x)
+
+
+let sumOfMultiplesTo sumTo multiples =
+    seq { 0..sumTo - 1 }
+    |> Seq.filter (isMultipleOfAnyIn multiples)
+    |> Seq.sum 
+
+
+let sumOfMultiplesTo' sumTo multiples =
+    let multiplesOf x =
+        async {
+            return seq { 0..sumTo }
+            |> Seq.takeWhile (fun i -> i * x < sumTo)
+            |> Seq.fold (fun list i ->
+                if list |> Seq.head < i * x
+                then i * x :: list 
+                else list
+            ) [0]
+        }
+
+    multiples
+    |> Seq.map multiplesOf
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Seq.concat
+    |> Seq.distinct
+    |> Seq.sum
+        
+    
+let answer = sumOfMultiplesTo 1000 [3; 5;] //233168
+let answer' = sumOfMultiplesTo' 1000 [3; 5;]
