edaf6ffc0b957e81028e79666aa9cfec00073328
Jeff Cortese <JCortese86@gmail.com>
2015-07-31 00:04:04 -0500

    bigSqrt WIP

--- FSharp.Euler/Problem3.LargestPrimeFactor.fs
+++ FSharp.Euler/Problem3.LargestPrimeFactor.fs
@@ -2,6 +2,8 @@
 
 module FSharp.Euler.LargestPrimeFactor
 
+open System.Numerics
+
 
 let isFactorOf (x: bigint) (y: bigint) = 
     if x < y then false
@@ -12,7 +14,7 @@ let isEven (x: bigint) =
     x % bigint(2) = bigint(0)
 
 
-let isPrime (x: bigint) =
+(*let isPrime (x: bigint) =
     if x < bigint(2) || isEven x then false
     else
         let until = if x < bigint(7) then bigint(7) else x
@@ -21,12 +23,39 @@ let isPrime (x: bigint) =
         |> Seq.filter (not << isEven)
         |> Seq.filter (fun i -> i <> x)
         |> Seq.forall (not << isFactorOf x)
+        *)
+
+let isPrime (n: BigInteger) =
+    let rec check i =
+        i > n/2I || (n % i <> 0I && check (i + 1I))
+    check 2I
         
 
-let largestPrimeFactorOf (x: bigint) =
-    seq { x..bigint(3) } // probably inefficient memory-wise, but i can't figure out how to exit a for loop early
-    |> Seq.takeWhile isPrime 
-    |> Seq.last
+(*let largestPrimeFactorOf (x: BigInteger) =
+    try
+        Seq.unfold (fun (i: BigInteger * bool) ->     
+            if fst i < 2I || snd i
+            then 
+                None 
+            else 
+                Some (i, (fst i - 1I, (isPrime <| fst i) && isFactorOf (fst i) x))
+            ) (x / 2I, false)
+        |> Seq.last |> fst
+
+    with
+        msg -> -1I*)
+
+
+let largestPrimeFactorOf (x: BigInteger) =
+    let isFactor (z: BigInteger) = 
+        x % z = 0I
+
+    let rec doIt (i: BigInteger) =
+        if isFactor i 
+        then true 
+        else doIt (i - 1I)
+
+    doIt (Common.bigSqrt x)
 
 
 let thing = bigint(600851475143L)
---
+++ FSharp.Euler/Program.fs
@@ -0,0 +1,10 @@
+﻿module Program
+
+open FSharp.Euler
+
+[<EntryPoint>]
+
+printfn "Problem3: %s" <| LargestPrimeFactor.answer.ToString ()
+
+System.Console.In.ReadLine () |> ignore
+()
