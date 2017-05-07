7fb13e4e3cebf48404335205fba3276ac1166da7
Jeff Cortese <JCortese86@gmail.com>
2017-05-07 04:56:58 -0500

    problem 4

---
+++ FSharp.Euler/Problem4.ThreeDigitProductPalindrome.fs
@@ -0,0 +1,13 @@
+﻿module FSharp.Euler.ThreeDigitProductPalindrome
+
+open System
+
+let isPalindrome (thing: string) =
+    (thing.ToCharArray() |> Array.rev |> fun chars -> new string(chars)) = thing
+
+let answer =
+    seq { for i = 999 downto 100 do
+            for j = 999 downto 100 do 
+                if (i * j).ToString() |> isPalindrome then yield i * j
+        }
+    |> Seq.max
--- FSharp.Euler/Program.fs
+++ FSharp.Euler/Program.fs
@@ -7,5 +7,6 @@ open FSharp.Euler
 printfn "Problem1: %d" <| MultiplesOf3And5.answer
 printfn "Problem2: %d" <| EvenFibonacciNumbers.answer
 printfn "Problem3: %d" <| LargestPrimeFactor.answer
+printfn "Problem4: %d" <| ThreeDigitProductPalindrome.answer
 
 System.Console.In.ReadLine () |> ignore
